a5c4bc714c6406929224e04b97ce895962aeee98
Arthas <arthaslixin1993@gmail.com>
2017-02-11 16:51:35 +0800

    Initial Commit

---
+++ BurnRateTests/BurnRateTests.swift
@@ -0,0 +1,36 @@
+//
+//  BurnRateTests.swift
+//  BurnRateTests
+//
+//  Created by Arthas on 2017/2/11.
+//  Copyright © 2017年 Arthas. All rights reserved.
+//
+
+import XCTest
+@testable import BurnRate
+
+class BurnRateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
